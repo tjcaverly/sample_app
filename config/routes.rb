2dc5f90976e791e2f56587cfa5d0108cb9d18b82
Trevor Caverly <trevorcaverly@gmail.com>
2014-03-19 13:40:11 -0400

    Add Contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
