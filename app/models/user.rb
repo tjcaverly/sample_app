27f6dc65f419fda1f6adefd4cb71e3b1c2a153bb
Trevor Caverly <trevorcaverly@gmail.com>
2014-05-13 15:21:11 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   before_save { email.downcase! }
   before_create :create_remember_token
 
@@ -7,6 +9,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false}
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,7 +51,7 @@ describe "Authentication" do
 
       describe "access new page" do
         before { visit signup_path }
-        it { should have_content('Welcome to the Sample App') }
+        it { should_not have_title('Sign up') }
       end
 
       describe "followed by signout" do
@@ -93,6 +93,19 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting the the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
 
